67bd292357b2a4e5f4b6c4adbb7bc73b99466ea4
lordtomi0325 <sungminchoidev@gmail.com>
2021-06-16 01:44:28 +0900

    edit ft_lstmap.c function

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -6,24 +6,32 @@
 /*   By: sungmcho <sungmcho@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/13 18:53:03 by sungmcho          #+#    #+#             */
-/*   Updated: 2021/06/15 18:22:47 by sungmcho         ###   ########.fr       */
+/*   Updated: 2021/06/16 01:44:07 by sungmcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+    t_list  *new;
+    t_list  *temp;
+    t_list  *last;
+
+    if (!(new = ft_lstnew(f(lst->content))))
+        return (NULL);
+    last = new;
+    lst = lst->next;
 	while (lst)
 	{
-		if(!f(lst->content))
-		{
-			ft_lstclear(&lst, del);
-			return (lst);
-		}
-		printf("%d\n", *(int *)(lst->content));
+	    if(!(temp = ft_lstnew(f(lst->content))))
+    	{
+    		ft_lstclear(&lst, del);
+	    	return (NULL);
+	    }
+        last->next = temp;
+        last = temp;
 		lst = lst->next;
 	}
-	return (lst);
+	return (new);
 }
