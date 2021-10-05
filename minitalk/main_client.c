fa2c0b1dd95716738ae7858892f8764518913f3b
lordtomi0325 <sungminchoidev@gmail.com>
2021-10-05 15:59:50 +0900

    signal process logic in main_client

--- minitalk/main_client.c
+++ minitalk/main_client.c
@@ -1,7 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_client.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sungmcho@student.42.kr <sungmcho@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/05 13:51:33 by sungmcho@st       #+#    #+#             */
+/*   Updated: 2021/10/05 15:25:04 by sungmcho@st      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "./ft_printf/ft_printf.h"
 #include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int	main(int ac, char **av)
+int	main()
 {
+	pid_t	pid;
+	char	ch;
+	void	f(int);
+	void	g(int);
+	int		i;
+
+	if (signal(SIGUSR1, f) == SIG_ERR)
+		ft_printF("can't catch SIGUSR1\n");
+	if (signal(SIGUSR2, g) == SIG_ERR)
+		ft_printf("cant't catch SIGUSR2\n");
+	while (1)
+		sleep(1);
+	// while (read(STDIN_FILENO, &ch, 1) > 0)
+	// {
+	// 	ft_printf("%c", ch);
+	// }
 	return (0);
+}
+
+void f(int singnum)
+{
+	ft_printf("Recieved a frog!\n");
+}
+
+void g(int singnum)
+{
+	ft_printf("Recieved a giraffe!\n");
+	exit(SIGUSR2);
 }
--- minitalk/main_server.c
+++ minitalk/main_server.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_server.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sungmcho@student.42.kr <sungmcho@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/05 13:51:28 by sungmcho@st       #+#    #+#             */
+/*   Updated: 2021/10/05 13:51:30 by sungmcho@st      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "./ft_printf/ft_printf.h"
 #include <signal.h>
 
@@ -5,6 +17,6 @@ int	main(void)
 {
 	char	*s1;
 
-	ft_printf("d\n", getpid());
+	ft_printf("%d\n", getpid());
 	return (0);
 }
