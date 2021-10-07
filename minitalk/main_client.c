5467387e1097a5cd992c684c4868c26433d89eec
lordtomi0325 <sungminchoidev@gmail.com>
2021-10-07 16:26:54 +0900

    remove printf folder and add static library

--- minitalk/main_client.c
+++ minitalk/main_client.c
@@ -6,43 +6,43 @@
 /*   By: sungmcho@student.42.kr <sungmcho@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 13:51:33 by sungmcho@st       #+#    #+#             */
-/*   Updated: 2021/10/05 15:25:04 by sungmcho@st      ###   ########.fr       */
+/*   Updated: 2021/10/07 13:51:46 by sungmcho@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./ft_printf/ft_printf.h"
+#include "./ft_printf.h"
 #include <signal.h>
 #include <unistd.h>
 #include <stdlib.h>
 
 int	main()
 {
-	pid_t	pid;
+	// pid_t	pid;
 	char	ch;
 	void	f(int);
 	void	g(int);
-	int		i;
+	// int		i;
 
 	if (signal(SIGUSR1, f) == SIG_ERR)
-		ft_printF("can't catch SIGUSR1\n");
+		ft_printf("can't catch SIGUSR1\n");
 	if (signal(SIGUSR2, g) == SIG_ERR)
 		ft_printf("cant't catch SIGUSR2\n");
 	while (1)
 		sleep(1);
-	// while (read(STDIN_FILENO, &ch, 1) > 0)
-	// {
-	// 	ft_printf("%c", ch);
-	// }
+	while (read(STDIN_FILENO, &ch, 1) > 0)
+	{
+		ft_printf("%c", ch);
+	}
 	return (0);
 }
 
-void f(int singnum)
+void f(int signum)
 {
-	ft_printf("Recieved a frog!\n");
+	ft_printf("%d, Recieved a frog!\n", signum);
 }
 
-void g(int singnum)
+void g(int signum)
 {
-	ft_printf("Recieved a giraffe!\n");
+	ft_printf("%d, Recieved a giraffe!\n", signum);
 	exit(SIGUSR2);
 }
--- minitalk/main_server.c
+++ minitalk/main_server.c
@@ -6,16 +6,16 @@
 /*   By: sungmcho@student.42.kr <sungmcho@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 13:51:28 by sungmcho@st       #+#    #+#             */
-/*   Updated: 2021/10/05 13:51:30 by sungmcho@st      ###   ########.fr       */
+/*   Updated: 2021/10/07 13:51:56 by sungmcho@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./ft_printf/ft_printf.h"
+#include "./ft_printf.h"
 #include <signal.h>
 
 int	main(void)
 {
-	char	*s1;
+	// char	*s1;
 
 	ft_printf("%d\n", getpid());
 	return (0);
