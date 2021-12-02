7c0082dfec86a2d34aa08ec77509183e129a1704
lordtomi0325 <sungminchoidev@gmail.com>
2021-12-02 22:18:35 +0900

    feat: add some lines in main_bonus.c

--- pipex/main_bonus.c
+++ pipex/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sungmcho <sungmcho@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 16:04:30 by sungmcho          #+#    #+#             */
-/*   Updated: 2021/12/01 16:21:00 by sungmcho         ###   ########.fr       */
+/*   Updated: 2021/12/02 15:17:47 by sungmcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,19 @@ void handle_error(void)
 int		main(int argc, char *argv[], char *env[])
 {
 	int pipes;
+	int fd[2];
+	int pid;
 
 	pipes = 0;
 	char *temp = env[0];
 	printf("%s\n", temp);
-	printf("%d\n", argc);
+	if (pipe(fd) < 0)
+		return 1;
 	if (argc < 5)
 		handle_error();
-	while (*argv)
+	while (argc < 3)
 	{
-		printf("%s\n", *argv);
-		argv++;
+		pid = fork();
 	}
 	return (0);
 }
