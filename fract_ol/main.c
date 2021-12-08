baaf506451e236702e92b9db90c65a9757b899b4
lordtomi0325 <sungminchoidev@gmail.com>
2021-12-08 20:59:57 +0900

    feat: add push swap folder and fract-ol main file

--- fract_ol/main.c
+++ fract_ol/main.c
@@ -1,62 +1,142 @@
-#include <mlx.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include	"mlx.h"
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<math.h>
 
-typedef struct s_vars
-{
-	void *mlx;
-	void *win;
-} t_vars;
+#define		X_EVENT_KEY_PRESS		2
+#define		X_EVENT_KEY_EXIT		17
+#define		X_EVENT_MOUSE_PRESS		4
+#define		X_EVENT_MOUSE_MOTION	6
+#define		WIN_WIDTH				800
+#define		WIN_HEIGHT				600
+#define		KEY_ESC					53
+#define		ITER_MAX				100
 
-typedef struct s_data
-{
-	void *img;
-	char *addr;
-	int bits_per_pixel;
-	int line_length;
-	int endian;
-} t_data;
+typedef	struct	s_img{
+	void		*img_ptr;
+	char		*data;
+	int			size_l;
+	int			bpp;
+	int			endian;
+}				t_img;
 
-int handle_exit(int keycode, t_vars *vars)
+typedef	struct	s_mlx{
+	void		*mlx_ptr;
+	void		*win;
+	t_img		img;
+}				t_mlx;
+
+int		mandelbrot(int count_w, int count_h, int iter)
 {
-	mlx_destroy_window(vars->mlx, vars->win);
-	exit(1);
+	double c_re;
+	double c_im;
+	double x;
+	double x_new;
+	double y;
+
+	c_re = ((count_w - WIN_WIDTH / 2) * 3.0 / WIN_WIDTH) - 0.5;
+	c_im = ((WIN_HEIGHT / 2) - count_h) * 2.0 / WIN_HEIGHT;
+	x = 0;
+	y = 0;
+	while ((pow(x, 2.0) + pow(y, 2.0) < 4) && (iter < ITER_MAX))
+	{
+		x_new = pow(x, 2.0) - pow(y, 2.0) + c_re;
+		y = 2 * x * y + c_im;
+		x = x_new;
+		iter++;
+	}
+	return (iter);
+}
+
+int		julia(int count_w, int count_h, int iter, t_img *img)
+{
+	double	c_re;
+	double	c_im;
+	double	x;
+	double	x_new;
+	double	y;
+
+	c_re = -0.75;
+	c_im = 0;
+	x = ((count_w - WIN_WIDTH / 2) * 4.0 / WIN_WIDTH);
+	y = ((WIN_HEIGHT / 2) - count_h) * 4.0 / WIN_HEIGHT;
+	while ((pow(x, 2.0) + pow(y, 2.0) < 4) && (iter < ITER_MAX))
+	{
+		x_new = pow(x, 2.0) - pow(y, 2.0) + c_re;
+		y = 2 * x * y + c_im;
+		x = x_new;
+		iter++;
+	}
+	return (iter);
+}
+
+int		window_init(t_mlx *mlx)
+{
+	if (!(mlx->mlx_ptr = mlx_init()))
+		return (0);
+	if (!(mlx->win = mlx_new_window(mlx->mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "A simple example")))
+		return (0);
+	if (!(mlx->img.img_ptr = mlx_new_image(mlx->mlx_ptr, WIN_WIDTH, WIN_HEIGHT)))
+		return (0);
+	if (!(mlx->img.data = mlx_get_data_addr(mlx->img.img_ptr, &mlx->img.bpp, &mlx->img.size_l, &mlx->img.endian)))
+		return (0);
 	return (1);
 }
 
-void my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 {
-	char *dst;
+	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = img->data + (x * img->bpp / 8) + (y * img->size_l);
 	*(unsigned int *)dst = color;
 }
 
-int main(int argc, char *argv[])
+void	put_pixel(t_img *img)
 {
-	t_vars vars;
-	t_data img;
-	int i;
-	int j;
-	int k;
+	int		iter;
+	int		count_w;
+	int		count_h;
 
-	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 640, 480, "fractol");
-	img.img = mlx_new_image(vars.mlx, 640, 480);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	for (i = 250, k = 99; i < 300; i++, k -= 2)
+	count_h = -1;
+	while (++count_h <= WIN_HEIGHT)
 	{
-		for (j = 150 + k; j < 250; j++)
-			my_mlx_pixel_put(&img, i, j, 0x0000FF00);
+		count_w = -1;
+		while (++count_w <= WIN_WIDTH)
+		{
+			iter = mandelbrot(count_w, count_h, 0);
+			//iter = julia(count_w, count_h, 0, img);
+			if (iter < ITER_MAX)
+				my_mlx_pixel_put(img, count_w, count_h, 0x00FFFFFF);
+			else
+				my_mlx_pixel_put(img, count_w, count_h, 0x00000000);
+		}
 	}
-	for (i = 300, k = 0; i < 350; i++, k += 2)
-	{
-		for (j = 150 + k; j < 250; j++)
-			my_mlx_pixel_put(&img, i, j, 0x0000FF00);
-	}
-	mlx_hook(vars.win, 2, 1L << 0, handle_exit, &vars);
-	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
-	mlx_loop(vars.mlx);
-	return 0;
+}
+
+int		key_press(int keycode)
+{
+	if (keycode == KEY_ESC)
+		exit(0);
+	else
+		return (0);
+	return(0);
+}
+
+int		close(int keycode)
+{
+	exit(0);
+}
+
+int		main(void)
+{
+	t_mlx	mlx;
+
+	if (!window_init(&mlx))
+		return (0);
+	put_pixel(&mlx.img);
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win, mlx.img.img_ptr, 0, 0);
+	mlx_hook(mlx.win, X_EVENT_KEY_PRESS, 0, key_press, 0);
+	mlx_hook(mlx.win, X_EVENT_KEY_EXIT, 0, close, 0);
+	mlx_loop(mlx.mlx_ptr);
+	return (0);
 }
