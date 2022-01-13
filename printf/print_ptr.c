a74fd56cb14bec093a29bf449d2a00f89714d60b
lordtomi0325 <sungminchoidev@gmail.com>
2022-01-13 14:48:21 +0900

    fix: fixing a print pointer func for 42 machine

--- printf/print_ptr.c
+++ printf/print_ptr.c
@@ -6,7 +6,7 @@
 /*   By: sungmcho <sungmcho@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 14:21:17 by sungmcho          #+#    #+#             */
-/*   Updated: 2022/01/13 14:29:19 by sungmcho         ###   ########.fr       */
+/*   Updated: 2022/01/13 14:48:15 by sungmcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,9 @@ void	print_ptr(va_list *args, int *res)
 	void	*c;
 
 	c = va_arg(*args, void *);
+	ft_putstr_fd("0x", 1, res);	
 	if (!c)
-	{
-		ft_putstr_fd("(nil)", 1, res);
-	}
+		ft_putstr_fd("0", 1, res);
 	else
-	{
-		ft_putstr_fd("0x", 1, res);
 		itoh((unsigned long long)c, 0, res);
-	}
 }
